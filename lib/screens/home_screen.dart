49e8047ce781a6c1d4d2b6b754e206646c47536a
Hardik Borda <hardik2197@users.noreply.github.com>
2020-12-03 23:42:52 +0530

    initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,389 @@
+import 'package:banking_app/constans/color_constant.dart';
+import 'package:banking_app/models/card_model.dart';
+import 'package:banking_app/models/operation_model.dart';
+import 'package:banking_app/models/transaction_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int current = 0;
+
+  List<T> map<T>(List list, Function handler) {
+    List<T> result = [];
+    for (var i = 0; i < list.length; i++) {
+      result.add(handler(i, list[i]));
+    }
+    return result;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: ListView(
+            physics: ClampingScrollPhysics(),
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 16, right: 16, top: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GestureDetector(
+                      child: SvgPicture.asset('assets/svg/drawer_icon.svg'),
+                    ),
+                    Container(
+                      height: 59,
+                      width: 59,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                          image: AssetImage('assets/images/user_image.png'),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 16, bottom: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Good Morning',
+                      style: GoogleFonts.inter(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                          color: kBlackColor),
+                    ),
+                    Text(
+                      'Amanda Alex',
+                      style: GoogleFonts.inter(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w700,
+                          color: kBlackColor),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 199,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  padding: EdgeInsets.only(left: 16, right: 6),
+                  itemCount: cards.length,
+                  itemBuilder: (context, index) => Container(
+                    height: 199,
+                    width: 344,
+                    margin: EdgeInsets.only(right: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(28),
+                      color: Color(cards[index].cardBackground),
+                    ),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          child: SvgPicture.asset(cards[index].cardElementTop),
+                        ),
+                        Positioned(
+                          right: 0,
+                          bottom: 0,
+                          child:
+                              SvgPicture.asset(cards[index].cardElementBottom),
+                        ),
+                        Positioned(
+                            left: 29,
+                            top: 48,
+                            child: Text(
+                              'CARD NUMBER',
+                              style: GoogleFonts.inter(
+                                  color: kWhiteColor,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400),
+                            )),
+                        Positioned(
+                          left: 29,
+                          top: 65,
+                          child: Text(
+                            cards[index].cardNumber,
+                            style: GoogleFonts.inter(
+                                color: kWhiteColor,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                        Positioned(
+                          right: 21,
+                          top: 35,
+                          child: Image.asset(
+                            cards[index].cardType,
+                            width: 27,
+                            height: 27,
+                          ),
+                        ),
+                        Positioned(
+                          left: 29,
+                          bottom: 45,
+                          child: Text(
+                            'CARD HOLDERNAME',
+                            style: GoogleFonts.inter(
+                                color: kWhiteColor,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ),
+                        Positioned(
+                          left: 29,
+                          bottom: 21,
+                          child: Text(
+                            cards[index].user,
+                            style: GoogleFonts.inter(
+                                color: kWhiteColor,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                        Positioned(
+                          left: 202,
+                          bottom: 45,
+                          child: Text(
+                            'EXPIRY DATE',
+                            style: GoogleFonts.inter(
+                                color: kWhiteColor,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400),
+                          ),
+                        ),
+                        Positioned(
+                          left: 202,
+                          bottom: 21,
+                          child: Text(
+                            cards[index].cardExpired,
+                            style: GoogleFonts.inter(
+                                color: kWhiteColor,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 16, bottom: 13, top: 29, right: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Options',
+                      style: GoogleFonts.inter(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w700,
+                          color: kBlackColor),
+                    ),
+                    Row(
+                      children: map<Widget>(
+                        datas,
+                        (index, selected) {
+                          return Container(
+                            alignment: Alignment.centerLeft,
+                            height: 9,
+                            width: 9,
+                            margin: EdgeInsets.only(right: 6),
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: current == index
+                                    ? kBlueColor
+                                    : kTwentyBlueColor),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 123,
+                child: ListView.builder(
+                    itemCount: datas.length,
+                    padding: EdgeInsets.only(left: 8, right: 8),
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) => GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              current = index;
+                            });
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 8),
+                            child: OperationCard(
+                              operation: datas[index].name,
+                              selectedIcon: datas[index].selectedIcon,
+                              unselectedIcon: datas[index].unselectedIcon,
+                              isSelected: current == index,
+                              context: this,
+                            ),
+                          ),
+                        )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 16, bottom: 13, top: 29, right: 10),
+                child: Text(
+                  'Transaction Histories',
+                  style: GoogleFonts.inter(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
+                      color: kBlackColor),
+                ),
+              ),
+              ListView.builder(
+                padding: EdgeInsets.only(left: 16, right: 16),
+                itemCount: transactions.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) => Container(
+                  height: 76,
+                  margin: EdgeInsets.only(bottom: 13),
+                  padding:
+                      EdgeInsets.only(left: 12, top: 12, bottom: 12, right: 10),
+                  decoration: BoxDecoration(
+                    color: kWhiteColor,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: kTenBlackColor,
+                        blurRadius: 10,
+                        spreadRadius: 5,
+                        offset: Offset(8, 8),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 57,
+                            width: 57,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                image: AssetImage(transactions[index].photo),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                transactions[index].name,
+                                style: GoogleFonts.inter(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w700,
+                                    color: kBlackColor),
+                              ),
+                              Text(
+                                transactions[index].date,
+                                style: GoogleFonts.inter(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w400,
+                                    color: kGreyColor),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      Text(
+                        transactions[index].amount,
+                        style: GoogleFonts.inter(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w700,
+                            color: kBlueColor),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class OperationCard extends StatefulWidget {
+  final String operation;
+  final String selectedIcon;
+  final String unselectedIcon;
+  final bool isSelected;
+  _HomeScreenState context;
+
+  OperationCard(
+      {this.operation,
+      this.selectedIcon,
+      this.unselectedIcon,
+      this.isSelected,
+      this.context});
+
+  @override
+  _OperationCardState createState() => _OperationCardState();
+}
+
+class _OperationCardState extends State<OperationCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 123,
+      height: 123,
+      decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: kTenBlackColor,
+              blurRadius: 10,
+              spreadRadius: 5,
+              offset: Offset(8, 8),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(15),
+          color: widget.isSelected ? kBlueColor : kWhiteColor),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+              widget.isSelected ? widget.selectedIcon : widget.unselectedIcon),
+          SizedBox(
+            height: 9,
+          ),
+          Text(
+            widget.operation,
+            textAlign: TextAlign.center,
+            style: GoogleFonts.inter(
+                fontSize: 15,
+                fontWeight: FontWeight.w700,
+                color: widget.isSelected ? kWhiteColor : kBlueColor),
+          )
+        ],
+      ),
+    );
+  }
+}
